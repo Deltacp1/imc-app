30f185fbd98dc22a2cb4a650b4667d9db9784ae2
Deltacp1 <rhernanni@gmail.com>
2025-02-01 23:17:48 -0300

    atualização inicial da pagina principal do calculo de imc

---
+++ src/imc_page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './imc_page.css';
+
+function imc_page(){
+    return (
+        <div className='imc-page'>
+            <h1>Calculadora de IMC</h1>
+            {/*Logica de entrada*/}
+        </div>
+    );
+}
+
+export default imc_page;
